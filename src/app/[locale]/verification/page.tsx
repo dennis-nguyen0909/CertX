72ed42ae0ec6a2ca7ae0fa2379276cb2b5cc8668
Minh Duy <dennis.nguyen0909@gmail.com>
2025-07-22 21:30:23 +0700

    feat: add metadata for certificate verification page; include title and description for improved SEO and user context

--- src/app/[locale]/verification/page.tsx
+++ src/app/[locale]/verification/page.tsx
@@ -1,5 +1,11 @@
 import CertificateVerifyPage from "@/containers/certificate-verify-page";
 
+export const metadata = {
+  title: "Xác Minh Chứng Chỉ & Văn Bằng STU - Xác Thực Nhanh Chóng",
+  description:
+    "Xác thực chứng chỉ và văn bằng của Trường Đại học STU (Đại học Công nghệ Sài Gòn) một cách dễ dàng và nhanh chóng. Đảm bảo tính hợp lệ của tài liệu.",
+};
+
 export default function Page() {
   return <CertificateVerifyPage />;
 }
